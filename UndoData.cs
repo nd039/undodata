643627abe408588583b48cc5d44d2dc1cdf09886
nd039 <wzalaznik@yahoo.com>
2016-08-01 04:59:12 -0500

    Add files via upload

---
+++ UndoData.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace databasesystems
+{
+	public class UndoData
+	{
+		private string buf;
+		private string columns;
+		private string _select;
+		private string _from;
+
+		public UndoData ()
+		{
+			buf = "";
+			columns = "";
+			_select = "";
+			_from = "";
+		}
+
+		public UndoData (string _buf)
+		{
+			buf = _buf;
+			columns = "RETURNS TABLE(";
+			_select = "SELECT ";
+			_from = "FROM ";
+		}
+
+		public UndoData (string _buf,string _columns, string __select, string __from)
+		{
+			buf = _buf;
+			columns = _columns;
+			_select = __select;
+			_from = __from;
+		}
+
+		public string Buf
+		{
+			get { return buf; }
+	        set { buf = value; }
+		}
+
+		public string Col
+		{
+			get { return columns; }
+			set {  columns = value; }
+		}
+
+		public string Sel
+		{
+			get { return _select; }
+	        set { _select = value; }
+		}
+
+		public string Fro
+		{
+			get { return _from; }
+	        set { _from = value; }
+		}
+	}
+}
+
